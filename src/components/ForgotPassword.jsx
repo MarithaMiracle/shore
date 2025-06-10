9ff605c8720379a44a942f4cc46b8d5786d0b379
Maritha Miracle <marithamiracle@gmail.com>
2025-06-11 00:50:52 +0100

    Fixes

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -13,7 +13,7 @@ const ForgotPassword = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch('http://localhost:5000/auth/forgot-password', {
+      const res = await fetch('https://estatify-gc8a.onrender.com/auth/forgot-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
